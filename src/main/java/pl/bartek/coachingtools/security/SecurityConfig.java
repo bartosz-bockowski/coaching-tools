014f9d5215306dc51ab6d39ba26823d25453cca4
Bartek Boćkowski <bboc12357@gmail.com>
2023-11-21 23:47:23 +0100

    basic messages and layout

--- src/main/java/pl/bartek/coachingtools/security/SecurityConfig.java
+++ src/main/java/pl/bartek/coachingtools/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.bartek.coachingtools.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,15 +13,17 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> {
                     auth
-                            .anyRequest().permitAll();
+                            .anyRequest().authenticated();
                 })
-                .formLogin((form) -> form.loginPage("/login").failureUrl("/login?error=true").permitAll());
+                .formLogin((form) -> form.loginPage("/login").failureUrl("/login?error=true").permitAll())
+                .logout((logout) -> logout.logoutUrl("/logout").permitAll());
         return http.build();
     }
 
