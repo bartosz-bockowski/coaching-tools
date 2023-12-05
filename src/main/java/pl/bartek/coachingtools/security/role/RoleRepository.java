d2f9b7446e35cd6c770809aee79c67dd5aef950f
Bartek Boćkowski <bboc12357@gmail.com>
2023-12-05 22:59:14 +0100

    more cleanups

--- src/main/java/pl/bartek/coachingtools/security/role/RoleRepository.java
+++ src/main/java/pl/bartek/coachingtools/security/role/RoleRepository.java
@@ -3,8 +3,6 @@ package pl.bartek.coachingtools.security.role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/pl/bartek/coachingtools/security/user/UserRepository.java
+++ src/main/java/pl/bartek/coachingtools/security/user/UserRepository.java
@@ -1,7 +1,6 @@
 package pl.bartek.coachingtools.security.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
@@ -12,5 +11,4 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>, QuerydslPredicateExecutor<User> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByRestorePasswordCode(String code);
 }
